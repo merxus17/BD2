6f9bdd5f2f53c6ead4e80d75a6d402915fd64296
rodrigo <rrangelvargas@gmail.com>
2019-05-01 20:11:21 -0300

    criadas as consultas

--- consultas.sql
+++ consultas.sql
@@ -1,9 +1,101 @@
-CREATE FUNCTION get_best_clientes (N in NUMBER)
-RETURNS 
+CREATE OR REPLACE FUNCTION criar_produto (Num INTEGER, Des CHAR(250), Qtd INTEGER)
+RETURNS VOID
+AS $$
+BEGIN
+INSERT INTO Mercadoria (NumeroMercadoria, Descricao, QuantidadeEstoque)
+VALUES (Num, Des, Qtd);
+END; $$
 
-SELECT * 
-FROM (SELECT Cliente_Fornecedor.Codigo,sum(ItensNota.QUANTIDADE * VALORUNITARIO) AS vendas 
-FROM NotasVenda JOIN ItensNota ON NotasVenda.numero = ItensNota.numero
-JOIN Cliente_Fornecedor on NotasVenda.CodigoCliente = Cliente_Fornecedor.Codigo 
-GROUP BY Cliente_Fornecedor.Codigo ORDER BY vendas DESC)
-WHERE rownum <= N
+LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION get_best_clientes (N INTEGER)
+RETURNS TABLE (
+    Codigo INTEGER,
+    Nome CHAR(150),
+    Numero INTEGER
+)
+AS $$
+BEGIN
+RETURN QUERY
+SELECT * FROM (
+    SELECT CLIENTE_FORNECEDOR.CODIGO,sum(ItensNota.quantidade * ValorUnitario) AS vendas 
+    FROM NotasVenda 
+    JOIN ItensNota ON NotasVenda.Numero = ItensNota.Numero
+    JOIN CLIENTE_FORNECEDOR ON NotasVenda.CodigoCliente = CLIENTE_FORNECEDOR.CODIGO 
+    GROUP BY CLIENTE_FORNECEDOR.CODIGO ORDER BY vendas DESC
+) AS derivedTable
+WHERE rownum <= N;
+END; $$ 
+ 
+LANGUAGE 'plpgsql';
+
+
+
+
+CREATE OR REPLACE FUNCTION get_cliente_fornecedor (ID INTEGER)
+RETURNS TABLE (
+  CODIGO INTEGER,
+  NOME CHAR(90) ,
+  TELEFONE CHAR(11) ,
+  LOGRADOURO CHAR(50) ,
+  NUMERO INTEGER ,
+  COMPLEMENTO CHAR(10) ,
+  CIDADE CHAR(15) ,
+  ESTADO CHAR(45) ,
+  NUMEROCONTRIBUINTE INTEGER,
+  CNPJ CHAR(18)
+)
+AS $$
+BEGIN
+SELECT * FROM CLIENTE_FORNECEDOR WHERE CODIGO=ID;
+END; $$
+
+LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION get_estoque_preco (NUMERO INTEGER)
+RETURNS TABLE (
+    NumeroMercadoria INTEGER,
+    QuantidadeEstoque INTEGER,
+    ValorUnitario INTEGER
+)
+AS $$
+BEGIN
+SELECT * FROM Mercadorias JOIN ItensNota ON NumeroMercadoria
+WHERE NumeroMercadoria=NUMERO;
+END; $$
+
+LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION get_info_pedido (N INTEGER)
+RETURNS TABLE (
+  NUMERO INTEGER,
+  DATAEMISSAO DATE ,
+  CODIGOFORNECEDOR INTEGER
+)
+AS $$
+BEGIN
+SELECT * FROM Notas_E_Compras WHERE NUMERO=N;
+END; $$
+
+LANGUAGE 'plpgsql';
+
+
+--  RETURNS TABLE (
+--  film_title VARCHAR,
+--  film_release_year INT
+-- ) 
+-- AS $$
+-- BEGIN
+--  RETURN QUERY SELECT
+--  title,
+--  cast( release_year as integer)
+--  FROM
+--  film
+--  WHERE
+--  title ILIKE p_pattern ;
+-- END; $$ 
+ 
+-- LANGUAGE 'plpgsql';
